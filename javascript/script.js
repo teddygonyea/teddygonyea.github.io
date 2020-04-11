0e6e3263929e163fd19c87a8a957d7c5c06567be
teddygonyea <enterted@gmail.com>
2020-04-10 23:21:46 -0400

    mobile 2

--- javascript/script.js
+++ javascript/script.js
@@ -2,8 +2,8 @@
    window.onload=function(){
    var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
    if (mobile) {
-       alert("You are using a mobile device. Please revisit on a computer");
+       window.location="http://teddysounds.com/mobile.html"
+
    } else {
-      alert("no");
    }
  }
