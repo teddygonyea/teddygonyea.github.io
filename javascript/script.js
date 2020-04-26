7b9dd3f48158924c728dc63386151346dec56824
teddygonyea <enterted@gmail.com>
2020-04-25 20:57:59 -0400

    music page

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,8 @@
 
-   window.onload=function(){
+window.onload=function(){
    var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
    if (mobile) {
        window.location="http://teddysounds.com/mobile.html"
-
    } else {
    }
  }
