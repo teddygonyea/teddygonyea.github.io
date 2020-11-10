b645f40e4de180fb4de6a05bdedbe376ccb58775
teddygonyea <62407086+teddygonyea@users.noreply.github.com>
2020-11-09 20:42:34 -0500

    Big new edits

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,6 @@
 
+/*
+
 window.onload=function(){
    var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
    if (mobile) {
@@ -6,3 +8,5 @@ window.onload=function(){
    } else {
    }
  }
+
+ */
